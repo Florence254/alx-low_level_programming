7be6b7042a8f820e5ba54e5ad0b37f8d22126e6b
Florence254 <soninjoroge70@gmail.com>
2023-03-23 07:07:06 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,3 +34,50 @@ char *_strcpy(char *dest, char *src)
 
 	return (dest);
 }
+
+/**
+ * new_dog - Program that creates a new dog. That is, creates a new struct
+ * @name: Stores the name char array
+ * @age: Stores the age float
+ * @owner: Stores the owner char array
+ * Return: Pointer to the newly created struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = NULL;
+	char *newName, *newOwner;
+	int newNameLen, newOwnerLen;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	newDog = malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+		return (NULL);
+
+	newNameLen = _strlen(name);
+	newName = malloc(sizeof(char *) * (newNameLen + 1));
+
+	if (newName == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+
+	newOwnerLen = _strlen(owner);
+	newOwner = malloc(sizeof(char *) * (newOwnerLen + 1));
+
+	if (newOwner == NULL)
+	{
+		free(newName);
+		free(newDog);
+		return (NULL);
+	}
+
+	newDog->name = _strcpy(newName, name);
+	newDog->age = age;
+	newDog->owner = _strcpy(newOwner, owner);
+
+	return (newDog);
+}
